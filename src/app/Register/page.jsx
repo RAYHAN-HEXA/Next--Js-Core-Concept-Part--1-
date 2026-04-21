7a33aadf27397fa3439471bd51f0348e8f3549f9
MD.RAYHAN <md.rayhanx617@gmail.com>
2026-04-21 19:49:08 +0600

    create login and register page

---
+++ src/app/Register/page.jsx
@@ -0,0 +1,14 @@
+import Title from '@/Components/Title';
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <Title>
+                Weelcome to REgister page 
+            </Title>
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/Components/Title';
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className='bg-linear-60 from-sky-200 to-sky-400 p-3 rounded text-black  ' >
+        <Title>Wellcome to login page  </Title>  
+        </div>
+    );
+};
+
+export default Login;
